d8818a1ad94746a9166429ffac46f9b70e2242a4
felipe <felipeavmuller@gmail.com>
2023-09-11 15:02:07 -0300

    add rules to js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const maxCount = 240;
+const maxCount = 280;
 const rad = 10;
 function countWords(inputValue) {
   const words = inputValue.trim().split(' ');
@@ -21,12 +21,22 @@ const characterCounterElement = document.getElementById('character-counter');
 textarea.addEventListener('input', () => {
   wordCounterElement.innerHTML = countWords(textarea.value);
   characterCounterElement.innerHTML = countCharacters(textarea.value);
-  const textarea2 = document.getElementsByTagName('textarea')[0];
-  textarea2.style.height = 'auto'; // Define a altura para automática
-  textarea2.style.height = textarea2.scrollHeight + 'px'; // Ajusta a altura com base no conteúdo
+
+  // Redefine a altura do textarea, de acordo com o conteúdo
+  textarea.style.height = 'auto'; // Define a altura para automática
+  textarea.style.height = `${textarea.scrollHeight}px`; // Ajusta a altura com base no conteúdo
 
   const circle = document.getElementById('inner');
-  const calculate = (2 * 3.141592653589793 * rad) - (((2 * 3.141592653589793 * rad) * countCharacters(textarea.value)) / maxCount)
-  console.log(calculate)
-  circle.style.strokeDashoffset = calculate;
+  const calculate = (2 * Math.PI * rad) - (((2 * Math.PI * rad) * countCharacters(textarea.value)) / maxCount)
+
+  if (countCharacters(textarea.value) >= maxCount) {
+    circle.style.stroke = '#f4212e';
+    circle.style.strokeDashoffset = 0;
+  } else if ((maxCount - countCharacters(textarea.value)) <= 20) {
+    circle.style.stroke = '#fcdc58';
+  } else {
+    circle.style.stroke = '#1f9bf0';
+    circle.style.strokeDashoffset = calculate;
+  }
+
 })
